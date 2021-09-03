6b5015bcdfd08155d93a029ff8a43c3d0d59b627
Mariano <mariano_farace@hotmail.com>
2021-09-03 19:49:04 +0200

    updated git ignore and changed baseURL to deployed server

--- part3/phonebook_frontend39-311/src/services/backendCom.js
+++ part3/phonebook_frontend39-311/src/services/backendCom.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3010/api/persons";
+const baseUrl = "https://quiet-retreat-13808.herokuapp.com/api/persons";
 
 const getAll = () => {
     return axios.get(baseUrl).then((response) => response.data);
