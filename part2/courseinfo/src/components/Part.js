da27504e1ceb15601de6afa0908130783e25924f
Mariano <mariano_farace@hotmail.com>
2021-09-16 13:39:16 +0200

    Remove nested git repo

--- part2/courseinfo/src/components/Part.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const Part = ({ parts }) => {
-  return parts.map((part) => (
-    <p key={part.name}> {part.name + " " + part.exercises} </p>
-  ));
-};
-
-export default Part;
