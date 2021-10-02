a6a3375cf91277e17716d68d568a881cf161923d
Mariano <mariano_farace@hotmail.com>
2021-10-02 14:06:34 +0200

    Before running ESLint

--- part4/BlogList/tests/node_api.test.js
+++ part4/BlogList/tests/node_api.test.js
@@ -1,24 +1,10 @@
-const { before } = require("lodash")
 const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
 const Blog = require("../models/blogModel")
 const api = (supertest(app))
+const helper = require("./test_helper")
 
-
-const initialBlogs = [{
-        title: "Primer blog del dest",
-        author: "bananero",
-        url: "wwww cagada.com",
-        likes: 132
-    },
-    {
-        title: "Segundo blog del dest",
-        author: "Willie",
-        url: "wwww.liberenlo.mx",
-        likes: 1000
-    }
-]
 
 
 beforeEach(
@@ -41,7 +27,7 @@ test("blogs are returned as json", async() => {
         .expect(200)
         .expect("Content-Type", /application\/json/)
 
-}, 10000000)
+})
 
 test("all blogs are returned", async() => {
     const response = await api.get("/api/blogs").expect("Content-Type", /application\/json/)
@@ -53,6 +39,53 @@ test("a specific blog is returned within the returned blogs", async() => {
     const response = await api.get("/api/blogs")
     const titles = response.body.map(r => r.title)
     expect(titles).toContainEqual("Primer blog del dest")
+})
+
+
+test("verifies that making an HTTP POST request to the /api/blogs url successfully creates a new blog post", async() => {
+
+    const newPost = {
+        title: "verifies that making an HTTP POST request",
+        author: "admin",
+        url: "https",
+        likes: 8
+    }
+
+
+    await api
+        .post("/api/blogs")
+        .send(newPost)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    //Test that the number of posts has been increased by one and the added post is the right one by title
+    const response = await api.get("/api/blogs")
+
+    console.log('type of response.body');
+    console.log(typeof response.body);
+    //console.log('response', response);
+    console.log('response.body', response.body);
+
+
+    const titles = await response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(titles).toContainEqual("verifies that making an HTTP POST request")
+})
+
+
+test("Blog wtihout content is not added.", async() => {
+
+    const newBlog = {}
+    await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(400)
+
+    const res = await api.get("/api/blogs")
+
+    expect(res.body).toHaveLength(initialBlogs.length)
+
 })
 
 afterAll(() => {
---
+++ part4/BlogList/tests/test_helper.js
@@ -0,0 +1,28 @@
+const Blog = require("../models/blogModel")
+
+const initialBlogs = [{
+        title: "Primer blog del dest",
+        author: "bananero",
+        url: "wwww cagada.com",
+        likes: 132
+    },
+    {
+        title: "Segundo blog del dest",
+        author: "Willie",
+        url: "wwww.liberenlo.mx",
+        likes: 1000
+    }
+]
+
+const blogsInDb = async() => {
+
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+
+}
+
+
+module.exports = {
+    initialBlogs,
+    blogsInDb
+}
