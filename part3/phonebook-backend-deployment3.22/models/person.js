03c6dcdb7506892f05a6189c0f6f5138cb0af75c
Mariano <mariano_farace@hotmail.com>
2022-02-23 20:30:51 +0100

    Implement eslint

--- part3/phonebook-backend-deployment3.22/models/person.js
+++ part3/phonebook-backend-deployment3.22/models/person.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.DBURL;
+const url = process.env.DBURL
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
   .then(() => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB", error.message);
-  });
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -20,27 +20,27 @@ const personSchema = new mongoose.Schema({
     unique: true,
     minLength: [
       5,
-      "Name must have at least 5 characeters. Sorry, no nicknames",
+      'Name must have at least 5 characeters. Sorry, no nicknames',
     ],
   },
   number: {
     type: String,
-    minLength: [8, "Phone number must have at least 8 characters"],
+    minLength: [8, 'Phone number must have at least 8 characters'],
     validate: {
       validator: function (v) {
-        return /^\d{2,3}[-]?\d{5,}/.test(v);
+        return /^\d{2,3}[-]?\d{5,}/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
