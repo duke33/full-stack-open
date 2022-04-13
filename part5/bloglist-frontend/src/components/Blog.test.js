0ba6cf05be1da19bff7afd09a488b79d08960c7e
Mariano <mariano_farace@hotmail.com>
2022-04-13 16:07:06 +0200

    Implemented first test

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,18 +4,23 @@ import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders content', () => {
-  const note = {
-    content: 'Component testing is done with react-testing-library',
-    important: true
+  const blog = {
+    _id: '616c03df1e0a061d5c9d309a',
+    title: 'Solo me borra mi dueño que es Mariano',
+    author: 'el middleware anda2',
+    url: 'wwwjwt.com',
+    likes: 78,
+    user: '616bd66581da029122bf51ff',
+
   }
+
 
   render(<Blog
     key={blog._id}
     blog={blog}
-    user={user}
-    setBlogs={setBlogs}
-    blogs={blogs} />)
+    user={blog.user}
+  />)
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
+  const element = screen.getByText( `${blog.title} - ${blog.author}`)
   expect(element).toBeDefined()
 })
