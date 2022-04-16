e76a169ba8ecd24402076d490b15ddc994570199
Mariano <mariano_farace@hotmail.com>
2022-04-16 15:25:40 +0200

    Login Test

--- part5/bloglist-frontend/cypress/integration/note_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/note_app.spec.js
@@ -1,7 +1,108 @@
 describe('Blog app', function() {
-  it('front page can be opened', function() {
+
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Mariano',
+      username: 'dule_33',
+      password: 'adksjlflkjsdflk'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
-    // cy.contains('Blogs')
-    // cy.contains('Blog app, Department of Computer Science, University of Helsinki 2022')
   })
+
+  it('Login form is shown', function() {
+    cy.contains('Log in to application')
+    cy.get('#username')
+    cy.get('#password')
+    cy.get('#login-button')
+
+  })
+
+  describe('Login',function() {
+
+
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('dule_33')
+      cy.get('#password').type('adksjlflkjsdflk')
+      cy.get('#login-button').click()
+      cy.contains('Mariano logged in')
+
+
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('Wrong')
+      cy.get('#password').type('Wrong')
+      cy.get('#login-button').click()
+      cy.contains('Wrong username or password')
+    })
+
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.get('#username').type('dule_33')
+      cy.get('#password').type('adksjlflkjsdflk')
+      cy.get('#login-button').click()
+      cy.contains('Mariano logged in')
+    })
+
+    it('A blog can be created be liked and deleted', function() {
+      cy.contains('create new blog').click()
+
+
+      cy.get('#title').type('Made with cypress')
+      cy.get('#author').type('Mariano')
+      cy.get('#url').type('https://www.cypress.io/')
+
+      cy.get('#create').click()
+      cy.contains('a new blog Made with cypress by Mariano added')
+
+      cy.get('#viewButton').click()
+
+      //Like a blog
+      cy.get('#likeButton').click()
+      cy.contains('likes 1')
+
+      //Delete a blog
+      cy.get('#removeButton').click()
+      cy.get('.blogLong').should('not.exist')
+
+
+    })
+
+    it.only('blogs are ordered according to likes with the blog with the most likes being first', function() {
+      //Create three blogs
+      cy.contains('create new blog').click()
+      cy.get('#title').type('0 Likes')
+      cy.get('#author').type('Mariano')
+      cy.get('#url').type('https://www.cypress.io/')
+      cy.get('#create').click()
+
+      cy.contains('create new blog').click()
+      cy.get('#title').type('3 likes')
+      cy.get('#author').type('Mariano')
+      cy.get('#url').type('https://www.cypress.io/')
+      cy.get('#create').click()
+
+      cy.contains('create new blog').click()
+      cy.get('#title').type('2 likes')
+      cy.get('#author').type('Mariano')
+      cy.get('#url').type('https://www.cypress.io/')
+      cy.get('#create').click()
+
+      cy.get('#viewButton').click()
+      cy.get('#likeButton').click()
+
+      cy.reload()
+
+
+
+    })
+
+
+
+  })
+
 })
