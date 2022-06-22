8aa90d860253c78d26ab94d9c9d2d21f57e3dfea
Mariano <mariano_farace@hotmail.com>
2022-06-22 11:01:49 +0200

    Refactor into components

---
+++ part6/Redux-notes/my-app/src/components/Notes.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { toggleImportanceOf } from "../reducers/noteReducer";
+import { useSelector, useDispatch } from "react-redux";
+
+const Notes = () => {
+  const Note = ({ note, handleClick }) => {
+    return (
+      <li key={note.id} onClick={handleClick}>
+        {note.content} <strong>{note.important ? "important" : ""}</strong>
+      </li>
+    );
+  };
+
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state);
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id));
+  };
+
+  return (
+    <div>
+      <ul>
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            handleClick={() => {
+              toggleImportanceOf(note.id);
+            }}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Notes;
