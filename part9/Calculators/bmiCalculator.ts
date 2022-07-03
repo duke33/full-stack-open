d749ab27e66982c63f4e7a40f2bae34a5f702243
Mariano <mariano_farace@hotmail.com>
2022-07-03 11:12:53 +0200

    Exercises 9.4.-9.5.

--- part9/Calculators/bmiCalculator.ts
+++ part9/Calculators/bmiCalculator.ts
@@ -1,28 +1,26 @@
-interface BodyValues {
-    height: number;
-    weight: number;
-  }
+// interface BodyValues {
+//     height: number;
+//     weight: number;
+//   }
 
-const parseArguments = (args: Array<string>): BodyValues => { 
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 4) throw new Error('Too many arguments');
+// const parseArguments = (args: Array<string>): BodyValues => { 
+//     if (args.length < 4) throw new Error('Not enough arguments');
+//     if (args.length > 4) throw new Error('Too many arguments');
   
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-      return {
-        height: Number(args[2]),
-        weight: Number(args[3])
-      }
-    } else {
-      throw new Error('Provided values were not numbers!');
-    }
+//     if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+//       return {
+//         height: Number(args[2]),
+//         weight: Number(args[3])
+//       }
+//     } else {
+//       throw new Error('Provided values were not numbers!');
+//     }
   
-  }
+//   }
 
-const calculateBmi = (height:number,weight:number):string=>{
-
+export const calculateBmi = (height:number,weight:number):string=>{
 const heightInMeters = height/100;
 const bmi = weight/(heightInMeters*heightInMeters);
-
 switch(true){
     case (bmi<18.5):
         return "Underweight";
@@ -30,19 +28,22 @@ switch(true){
         return "Normal";
      case bmi<30:
         return "Overweight";
+        case bmi>30:
+          return "Obese";
     default:
-        return "Obese";
+          throw new Error('Provided values were not numbers!');
 }
 
 }
 
-try {
-    const { height, weight } = parseArguments(process.argv);
-    console.log(calculateBmi(height, weight));
-  } catch (error: unknown) {
-    let errorMessage = 'Something bad happened.'
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    console.log(errorMessage);
-  }
+// try {
+//     const { height, weight } = parseArguments(process.argv);
+//     console.log(calculateBmi(height, weight));
+//   } catch (error: unknown) {
+//     let errorMessage = 'Something bad happened.'
+//     if (error instanceof Error) {
+//       errorMessage += ' Error: ' + error.message;
+//     }
+//     console.log(errorMessage);
+//   }
+
---
+++ part9/Calculators/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+const app = express();
+import {calculateBmi} from './bmiCalculator';
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/ping', (_req, res) => {
+  res.send('Hello Full Stack');
+});
+
+app.get("/bmi",(req, res) => {
+
+
+const height=parseInt(req.query.height as string);
+const weight=parseInt(req.query.weight as string);
+try{
+const bmi= calculateBmi(height,weight)
+res.json({
+  weight,
+  height,
+  bmi,
+});
+}catch(error:any){
+  res.send({
+    error: error.message
+  });
+}})
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
